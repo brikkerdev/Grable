ef7b4f12874d221f1ad693faa7e5d9de267d3afa
Tolex1510 <bogtelegin.2018@yandex.ru>
2025-11-21 09:38:19 +0300

    Add Progress Fragment

---
+++ app/src/main/java/ru/sirius/grable/ProgressFragment.kt
@@ -0,0 +1,22 @@
+package ru.sirius.grable
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ProgressFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_progress, container, false)
+    }
+}
