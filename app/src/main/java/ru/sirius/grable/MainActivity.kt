d130fa24ccb65326b9771601ac068195dbf93785
Tolex1510 <bogtelegin.2018@yandex.ru>
2025-11-21 09:39:45 +0300

    Change MainActivity

--- app/src/main/java/ru/sirius/grable/MainActivity.kt
+++ app/src/main/java/ru/sirius/grable/MainActivity.kt
@@ -1,6 +1,7 @@
 package ru.sirius.grable
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,6 +10,14 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            if (savedInstanceState == null) {
+                supportFragmentManager.beginTransaction()
+                    .add(R.id.fragment_container_view, ProgressFragment::class.java, null)
+                    .commit();
+            }
+        }
     }
 }
--- app/src/main/java/ru/sirius/grable/ProgressFragment.kt
+++ app/src/main/java/ru/sirius/grable/ProgressFragment.kt
@@ -16,7 +16,6 @@ class ProgressFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.activity_progress, container, false)
     }
 }
