d3e02d4b245dc6e86877088a344fe844f7c2e734
vinotq <vinotq.maim@gmail.com>
2025-11-25 17:07:23 +0300

    Added Categories navigation from main page Removed Header from anywhere except MainPage

--- app/src/main/java/ru/sirius/grable/main/HomeFragment.kt
+++ app/src/main/java/ru/sirius/grable/main/HomeFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import ru.sirius.grable.MainActivity
 import ru.sirius.grable.R
+import ru.sirius.grable.learn.LearnFragment
+import ru.sirius.grable.playlist.SelectPlaylistFragment
 
 class HomeFragment : Fragment() {
 
@@ -18,5 +22,16 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button_repeat)?.setOnClickListener {
+            (activity as? MainActivity)?.switchFragment(LearnFragment())
+        }
+
+        view.findViewById<Button>(R.id.button_collection)?.setOnClickListener {
+        }
+
+        view.findViewById<Button>(R.id.button_categories)?.setOnClickListener {
+            (activity as? MainActivity)?.switchFragment(SelectPlaylistFragment())
+        }
     }
 }
---
+++ app/src/main/java/ru/sirius/grable/playlist/PlaylistAdapter.kt
@@ -0,0 +1,33 @@
+package ru.sirius.grable.main
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import ru.sirius.grable.R
+
+class PlaylistAdapter(
+    private val playlists: List<Playlist>,
+    private val onItemClick: (Playlist) -> Unit
+) : RecyclerView.Adapter<PlaylistAdapter.PlaylistViewHolder>() {
+
+    class PlaylistViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val nameText: TextView = itemView.findViewById(R.id.playlist_name)
+        val descText: TextView = itemView.findViewById(R.id.playlist_description)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlaylistViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_playlist, parent, false)
+        return PlaylistViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: PlaylistViewHolder, position: Int) {
+        val playlist = playlists[position]
+        holder.nameText.text = playlist.name
+        holder.descText.text = playlist.description ?: "${playlist.name} description"
+        holder.itemView.setOnClickListener { onItemClick(playlist) }
+    }
+
+    override fun getItemCount() = playlists.size
+}
