055796f4480d478f0e20676dcfbb5d4b630656bd
BanderoL <ganshin1vladislav@gmail.com>
2025-11-25 16:07:50 +0300

    refactor: change style to textAppearance instead

--- app/src/main/java/ru/sirius/grable/main/HomeViewModel.kt
+++ app/src/main/java/ru/sirius/grable/main/HomeViewModel.kt
@@ -1,9 +1,26 @@
 package ru.sirius.grable.main
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
-//data class Category
+data class Playlist (
+    val id: String,
+    val name: String
+)
+
+data class State (
+    val playlists: List<Playlist>
+)
 
 class HomeViewModel : ViewModel() {
-    
+    private val _state = MutableStateFlow(State(listOf()))
+    val state = _state.asStateFlow()
+
+    fun getPlaylists() {
+        _state.value = _state.value.copy(
+            playlists = listOf(Playlist("food", "Еда"), Playlist("medicine", "Медицина"),
+                Playlist("dictionaryB2", "Словарь B2"), Playlist("dictionaryC1", "Словарь C1"))
+        )
+    }
 }
