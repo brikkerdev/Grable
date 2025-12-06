0eb4657278d0f97175819fd25ccf17d8f4758c3b
vinotq <vinotq.maim@gmail.com>
2025-12-06 17:08:29 +0300

    Resolved KSP DB conflict

--- app/src/main/java/ru/sirius/grable/common/Database.kt
+++ app/src/main/java/ru/sirius/grable/common/Database.kt
@@ -4,29 +4,37 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
+@TypeConverters(Converters::class)
 @Database(
-    entities = [ExampleEntity::class],
+    entities = [
+        ExampleEntity::class,
+        WordEntity::class,
+        PlaylistEntity::class,
+        StatisticsEntity::class
+    ],
     version = 1,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
-//    abstract fun dao
     companion object {
         @Volatile
         private var INSTANCE: AppDatabase? = null
-    fun getDatabase(context: Context): AppDatabase {
-        return INSTANCE ?: synchronized(this) {
-            createDatabase(context)
-                .also { db -> INSTANCE = db }
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                createDatabase(context)
+                    .also { db -> INSTANCE = db }
+            }
         }
-    }
-    private fun createDatabase(context: Context): AppDatabase {
-        return Room.databaseBuilder(
-            context.applicationContext,
-            AppDatabase::class.java,
-            "app_database"
-        ).build()
-    }
+
+        private fun createDatabase(context: Context): AppDatabase {
+            return Room.databaseBuilder(
+                context.applicationContext,
+                AppDatabase::class.java,
+                "app_database"
+            ).build()
+        }
     }
 }
