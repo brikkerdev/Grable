babfcf87fc1f8b29e6143e528a262a166763f2f2
milofon037 <anastasiagord037@gmail.com>
2025-12-22 15:22:49 +0300

    fix SelectPlaylistFragment

--- app/src/main/java/ru/sirius/grable/playlist/ui/SelectPlaylistFragment.kt
+++ app/src/main/java/ru/sirius/grable/playlist/ui/SelectPlaylistFragment.kt
@@ -4,19 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.launch
-import com.facebook.shimmer.ShimmerFrameLayout
 import ru.sirius.grable.MainActivity
-import ru.sirius.grable.R
 import ru.sirius.grable.databinding.FragmentSelectPlaylistBinding
 import ru.sirius.grable.learn.ui.LearnPlaylistFragment
 import ru.sirius.grable.main.HomeFragment
@@ -43,12 +37,6 @@ class SelectPlaylistFragment : Fragment() {
         }
     }
 
-    private val playlistRecyclerView by lazy { requireView().findViewById<RecyclerView>(R.id.playlist_items) }
-    private val playlistTitle by lazy { requireView().findViewById<TextView>(R.id.page_name) }
-    private val backButton by lazy { requireView().findViewById<Button>(R.id.button_back) }
-    private val playlistSkeleton by lazy { requireView().findViewById<ShimmerFrameLayout>(R.id.playlist_skeleton) }
-    private val contentGroup by lazy { requireView().findViewById<LinearLayout>(R.id.content_group) }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -70,10 +58,11 @@ class SelectPlaylistFragment : Fragment() {
         observeViewModel()
     }
 
-    private fun setupRecyclerView(view: View) {
+    private fun setupRecyclerView() {
         val recyclerView = binding.playlistItems
         recyclerView.adapter = adapter
         recyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
+    }
 
     private fun observeViewModel() {
         viewLifecycleOwner.lifecycleScope.launch {
@@ -87,25 +76,25 @@ class SelectPlaylistFragment : Fragment() {
     }
 
     private fun applyLoading(isLoading: Boolean) {
-        playlistSkeleton.isVisible = isLoading
-        contentGroup.isVisible = !isLoading
+        binding.playlistSkeleton.isVisible = isLoading
+        binding.contentGroup.isVisible = !isLoading
 
         if (isLoading) {
-            playlistSkeleton.startShimmer()
+            binding.playlistSkeleton.startShimmer()
         } else {
-            playlistSkeleton.stopShimmer()
+            binding.playlistSkeleton.stopShimmer()
         }
     }
 
     override fun onResume() {
         super.onResume()
-        if (playlistSkeleton.isVisible) {
-            playlistSkeleton.startShimmer()
+        if (binding.playlistSkeleton.isVisible) {
+            binding.playlistSkeleton.startShimmer()
         }
     }
 
     override fun onPause() {
-        playlistSkeleton.stopShimmer()
+        binding.playlistSkeleton.stopShimmer()
         super.onPause()
     }
 }
